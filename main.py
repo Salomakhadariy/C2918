e339a21c18a3fe57c41268ec9a9b62eb1ad76483
DariySalomakha <132147025+DariySalomakha@users.noreply.github.com>
2023-05-20 19:06:59 +0300

    qwe

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
 import random
 
 
+<<<<<<< Updated upstream
 
 
+=======
+# class Human:
+#     def __init__(self, name="Human"):
+#         self.name = name
+#
+#
+# class Auto:
+#     def __init__(self, brand):
+#         self.brand = brand
+#         self.passengers = []
+#
+#     def add_passenger(self, human):
+#         self.passengers.append(human)
+#
+#     def print_passengers_names(self):
+#         if self.passengers != []:
+#             print(f"Names of {self.brand} passengers: ")
+#             for passenger in self.passengers:
+#                 print(passenger.name)
+#         else:
+#             print(f"There are no passengers in {self.brand}")
+#
+# vasya = Human("Vasya")
+# petya = Human("Petya")
+#
+# car = Auto("Mercedes")
+#
+# car.add_passenger(vasya)
+# car.add_passenger(petya)
+# car.print_passengers_names()
+
+
+# ____________________SIMS__________________________
+
+>>>>>>> Stashed changes
 class Human:
     def __init__(self, name="Human", job=None, home=None, car=None):
         self.name = name
